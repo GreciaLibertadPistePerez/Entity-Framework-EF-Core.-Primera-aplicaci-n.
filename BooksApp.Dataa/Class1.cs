ac7ec96dace86c5e2e3a56b0942230ea3f3fb55d
Grecia Libertad Pisté Pérez <19470356@itcampeche.edu.mx>
2021-01-28 13:26:42 -0600

    Version inicial del proyecto.

---
+++ BooksApp.Dataa/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BooksApp.Dataa
+{
+    public class Class1
+    {
+    }
+}
---
+++ BooksApp.Domaiin/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BooksApp.Domaiin
+{
+    public class Class1
+    {
+    }
+}
